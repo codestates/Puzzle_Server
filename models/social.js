aa4c864e0780aecdf743edbbabb79f994af3d82c
unow30 <yunho7205@gmail.com>
2021-01-21 11:37:12 +0900

    refactor js4

--- models/social.js
+++ models/social.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      social.belongsTo(models.User, {
+      social.belongsTo(models.user, {
         onDelete: 'CASCADE',
         foreignKey: {
           allowNull: true,
