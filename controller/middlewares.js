6f1d7dc219ce36803d499fdb9bc85a1beb6b411b
kys88 <ys.sung106@gmail.com>
2021-01-24 13:05:23 +0900

    fix/config.js

--- controller/middlewares.js
+++ controller/middlewares.js
@@ -13,12 +13,3 @@ exports.isLoggedIn = (req, res, next) => {
 };
 //로그인했는지 확인하려면?
   //헤더에 토큰이 담겨있는지 확인 req.headers('Authentication')
-
-exports.isNotLoggedIn = (req, res, next) => {
-  if (req.headers('Authorization').split(' ')[1].length === 0) {
-    next();
-  } else {
-    const message = encodeURIComponent('로그인한 상태입니다.');
-    res.redirect(`/?error=${message}`);
-  }
-};
