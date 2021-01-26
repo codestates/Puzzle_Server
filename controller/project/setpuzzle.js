461a4e47c9b3b66973c92957a548894dace85d5a
unow30 <yunho7205@gmail.com>
2021-01-26 17:21:42 +0900

    feat/project/invite,setpuzzle,update
    complete /project

--- controller/project/setpuzzle.js
+++ controller/project/setpuzzle.js
@@ -2,18 +2,29 @@ const {
     isAuthorized
 } = require('../tokenFunctions')
 const { project } = require('../../models')
-
+//프로젝트의 이미지 퍼즐조각 개수 설정
 module.exports = async (req, res) => {
-    const { id, puzzleNum, projectImg, title, description } = req.body
-    const verifiedToken = isAuthorized(req)
-    if (!verifiedToken) {
-        res.status(401).json({ "error": "not authorized" })
+    const { id, puzzleNum } = req.body
+    if (!id || !puzzleNum) {
+        req.status(422).json({ "error": "you need to fill input" })
     } else {
-        const projectInfo = await project.update(req.body, {
-            where: { id: projectId }
-        })
 
+        const verifiedToken = isAuthorized(req)
+        if (!verifiedToken) {
+            res.status(401).json({ "error": "not authorized" })
+        } else {
+            const projectInfo = await project.update({
+                puzzleNum: puzzleNum
+            }, {
+                where: { id: id }
+            })
+
+            if (!projectInfo) {
+                res.status(404).json({ "error": "can't set puzzle" })
+            } else {
+                res.status(200).json({ "message": "ok" })
+            }
+        }
 
     }
-
 }
