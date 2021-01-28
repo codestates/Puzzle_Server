30a07d77eb0418d07d0dafd1f927508f402b0254
kys88 <ys.sung106@gmail.com>
2021-01-28 22:31:58 +0900

    feat/projectImgUpload

--- controller/project/setpuzzle.js
+++ controller/project/setpuzzle.js
@@ -1,30 +1,39 @@
 const {
     isAuthorized
 } = require('../tokenFunctions')
-const { project } = require('../../models')
-//프로젝트의 이미지 퍼즐조각 개수 설정
+const { userPermission, project } = require('../../models')
+//프로젝트 이미지 업로드 
 module.exports = async (req, res) => {
-    const { id, puzzleNum } = req.body
-    if (!id || !puzzleNum) {
-        req.status(422).json({ "error": "you need to fill input" })
-    } else {
-
-        const verifiedToken = isAuthorized(req)
-        if (!verifiedToken) {
-            res.status(401).json({ "error": "not authorized" })
-        } else {
-            const projectInfo = await project.update({
-                puzzleNum: puzzleNum
-            }, {
-                where: { id: id }
-            })
-
-            if (!projectInfo) {
-                res.status(404).json({ "error": "can't set puzzle" })
-            } else {
-                res.status(200).json({ "message": "ok" })
+    const verifiedToken = isAuthorized(req);
+    const projectId = req.params.id;
+    if (!verifiedToken) {
+        res.status(404).json( {"message": "invalid token"})
+    }else {
+        const { id } = verifiedToken;
+        //projectId가 요청한 유저가 팀으로 있는 프로젝트의 id인지 확인
+        const connection = await userPermission.findOne({
+            where: {
+                userId: id,
+                projectId: projectId
             }
-        }
+        })
+        if (!connection) {
+            res.status(404).json({"message": "You are not a member of this project"})
+        }else {
+            const updatedProject = await project.update({
+            prjectImg: req.file.location
+            }, {
+                where: { id: projectId }
+            })
+            if (!updatedProject) {
+                res.json({"message": "There are no project with matching projectId"})
+            }else {
+                res.status(202).send({"projectImgUrl": req.file.location})
+            }
 
+        }
     }
 }
+
+//S3에 프로젝트 이미지 업로드하고, req.params.id로 프로젝트 특정한 후에
+//그 프로젝트의 projectimg에 이미지url을 update
