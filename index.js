390a23042db1277ab08b2212e26ad556f1c3204e
kys88 <ys.sung106@gmail.com>
2021-04-29 13:49:00 +0900

    fix/addOrigin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const key = fs.readFileSync("./key.pem", "utf-8")
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "https://www.teampuzzle.ga" ],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://www.teampuzzle.ga", "https://puzzletool.link/" ],
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
   })
